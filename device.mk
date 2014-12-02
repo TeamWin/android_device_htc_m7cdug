116ed94ce22f4b6b7812bc6d8981a546e00d74a6
Bruno Martins <bgcngm@gmail.com>
2014-12-02 21:24:47 +0000

    Added off-mode charging support

--- device.mk
+++ device.mk
@@ -17,7 +17,16 @@ endif
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/fstab.m7c:recovery/root/fstab.m7cdug
+    $(LOCAL_PATH)/recovery/etc/fstab.m7c:recovery/root/fstab.m7cdug \
+    $(LOCAL_PATH)/recovery/etc/init.recovery.m7c.rc:root/init.recovery.m7cdug.rc \
+    $(LOCAL_PATH)/recovery/sbin/choice_fn:recovery/root/sbin/choice_fn \
+    $(LOCAL_PATH)/recovery/sbin/detect_key:recovery/root/sbin/detect_key \
+    $(LOCAL_PATH)/recovery/sbin/offmode_charging:recovery/root/sbin/offmode_charging \
+    $(LOCAL_PATH)/recovery/sbin/power_test:recovery/root/sbin/power_test
+
+PRODUCT_PACKAGES += \
+    offmode_charging_res_images \
+    offmode_charging_warn_res_images
 
 $(call inherit-product, build/target/product/full.mk)
 
